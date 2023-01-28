efc3e5b841a25ab81002224cc8cbddbde3132ff8
samaraCassie <samaracassie@outlook.com>
2023-01-28 02:43:42 -0300

    Create postHome

--- template/src/controllers/homeController.js
+++ template/src/controllers/homeController.js
@@ -1,5 +1,11 @@
 exports.getHome = (req, res) => {
     res.render('index');
 }
-
+exports.postHome =(req, res) => {
+    const body = req.body;
+    const nameUser = body.Nome;
+    const yearUser = body.Idade;
+    console.log(nameUser, yearUser);
+    res.send(`Obrigada pela informção trouxa :D<br> Nome do Usuário: ${body.Nome} <br> Idade do Usuário: ${body.Idade}`);
+}
    
