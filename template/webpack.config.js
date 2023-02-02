bd71aad19e9981cfab0b5b05e103b2a0b344eda5
samaraCassie <samaracassie@outlook.com>
2023-02-02 18:59:19 -0300

    Update webpack.config.js

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -5,30 +5,22 @@ module.exports = {
   entry: './frontend/main.js',
   output: {
     path: path.resolve(__dirname, 'public', 'assets', 'js'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        test: /\.js$/.css$/i,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    esmodules: true,
-                  },
-                },
-              ],
-            ],
-          },
-        },
-      },
-    ],
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
   },
-  devtool: 'source-map',
+  devtool: 'source-map'
 };
